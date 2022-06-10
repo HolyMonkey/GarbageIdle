85779eb541e7a7bb572b91cd3d28b33053937c38
Listopadov Evgeny <Listopadov_94@mail.ru>
2022-06-10 13:54:00 +0300

    вывод денег при выносе мусора

--- Assets/Scripts/PointFinish.cs
+++ Assets/Scripts/PointFinish.cs
@@ -1,22 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-[RequireComponent(typeof(ParticleSystem))]
 public class PointFinish : MonoBehaviour
 {
-    ParticleSystem _particleSystem;
-
-    private void Start()
-    {
-        _particleSystem = GetComponent<ParticleSystem>();
-    }
+    [SerializeField] private TMP_Text _textMoney;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.TryGetComponent(out Worker worker))
         {
-            _particleSystem.Play();
+            _textMoney.alpha = Mathf.Lerp(0, 1, 0.5f);
+            _textMoney.text ="+" + worker.Target.Price.ToString();
+            _textMoney.alpha = Mathf.Lerp(1, 0, 0.5f);
         }
     }
 }
